35e3f9b782459a8da5429305c6324d826576a7bc
joost-stessens <joost.stessens@hotmail.com>
2026-03-27 09:49:32 +0100

    Chore: replace original fonts cdn

--- scripts/woff2/woff2-vite-plugins.js
+++ scripts/woff2/woff2-vite-plugins.js
@@ -1,5 +1,4 @@
 // define `EXCALIDRAW_ASSET_PATH` as a SSOT
-const OSS_FONTS_CDN = "https://excalidraw.nyc3.cdn.digitaloceanspaces.com/oss/";
 const OSS_FONTS_FALLBACK = "/";
 
 /**
@@ -9,12 +8,14 @@ const OSS_FONTS_FALLBACK = "/";
  */
 module.exports.woff2BrowserPlugin = () => {
   let isDev;
+  let fontsBaseUrl = "/";
 
   return {
     name: "woff2BrowserPlugin",
     enforce: "pre",
-    config(_, { command }) {
+    config(config, { command }) {
       isDev = command === "serve";
+      fontsBaseUrl = config.base || "/";
     },
     transform(code, id) {
       // using copy / replace as fonts defined in the `.css` don't have to be manually copied over (vite/rollup does this automatically),
@@ -24,7 +25,7 @@ module.exports.woff2BrowserPlugin = () => {
 
       @font-face {
         font-family: "Assistant";
-        src: url(${OSS_FONTS_CDN}fonts/Assistant/Assistant-Regular.woff2)
+        src: url(${fontsBaseUrl}fonts/Assistant/Assistant-Regular.woff2)
             format("woff2"),
           url(./Assistant-Regular.woff2) format("woff2");
         font-weight: 400;
@@ -34,7 +35,7 @@ module.exports.woff2BrowserPlugin = () => {
 
       @font-face {
         font-family: "Assistant";
-        src: url(${OSS_FONTS_CDN}fonts/Assistant/Assistant-Medium.woff2)
+        src: url(${fontsBaseUrl}fonts/Assistant/Assistant-Medium.woff2)
             format("woff2"),
           url(./Assistant-Medium.woff2) format("woff2");
         font-weight: 500;
@@ -44,7 +45,7 @@ module.exports.woff2BrowserPlugin = () => {
 
       @font-face {
         font-family: "Assistant";
-        src: url(${OSS_FONTS_CDN}fonts/Assistant/Assistant-SemiBold.woff2)
+        src: url(${fontsBaseUrl}fonts/Assistant/Assistant-SemiBold.woff2)
             format("woff2"),
           url(./Assistant-SemiBold.woff2) format("woff2");
         font-weight: 600;
@@ -54,7 +55,7 @@ module.exports.woff2BrowserPlugin = () => {
 
       @font-face {
         font-family: "Assistant";
-        src: url(${OSS_FONTS_CDN}fonts/Assistant/Assistant-Bold.woff2)
+        src: url(${fontsBaseUrl}fonts/Assistant/Assistant-Bold.woff2)
             format("woff2"),
           url(./Assistant-Bold.woff2) format("woff2");
         font-weight: 700;
@@ -67,9 +68,8 @@ module.exports.woff2BrowserPlugin = () => {
         return code.replace(
           "<!-- PLACEHOLDER:EXCALIDRAW_APP_FONTS -->",
           `<script>
-        // point into our CDN in prod, fallback to root (excalidraw.com) domain in case of issues
+        // use app-root as runtime font path fallback (avoid duplicating BASE_URL prefixes)
         window.EXCALIDRAW_ASSET_PATH = [
-          "${OSS_FONTS_CDN}",
           "${OSS_FONTS_FALLBACK}",
         ];
       </script>
@@ -77,7 +77,7 @@ module.exports.woff2BrowserPlugin = () => {
       <!-- Preload all default fonts to avoid swap on init -->
       <link
         rel="preload"
-        href="${OSS_FONTS_CDN}fonts/Excalifont/Excalifont-Regular-a88b72a24fb54c9f94e3b5fdaa7481c9.woff2"
+        href="${fontsBaseUrl}fonts/Excalifont/Excalifont-Regular-a88b72a24fb54c9f94e3b5fdaa7481c9.woff2"
         as="font"
         type="font/woff2"
         crossorigin="anonymous"
@@ -85,14 +85,14 @@ module.exports.woff2BrowserPlugin = () => {
       <!-- For Nunito only preload the latin range, which should be good enough for now -->
       <link
         rel="preload"
-        href="${OSS_FONTS_CDN}fonts/Nunito/Nunito-Regular-XRXI3I6Li01BKofiOc5wtlZ2di8HDIkhdTQ3j6zbXWjgeg.woff2"
+        href="${fontsBaseUrl}fonts/Nunito/Nunito-Regular-XRXI3I6Li01BKofiOc5wtlZ2di8HDIkhdTQ3j6zbXWjgeg.woff2"
         as="font"
         type="font/woff2"
         crossorigin="anonymous"
       />
       <link
         rel="preload"
-        href="${OSS_FONTS_CDN}fonts/ComicShanns/ComicShanns-Regular-279a7b317d12eb88de06167bd672b4b4.woff2"
+        href="${fontsBaseUrl}fonts/ComicShanns/ComicShanns-Regular-279a7b317d12eb88de06167bd672b4b4.woff2"
         as="font"
         type="font/woff2"
         crossorigin="anonymous"
